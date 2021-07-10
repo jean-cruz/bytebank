e4a47fd8951571cee29d96c3b5d439eb105c209b
Jean Cruz <jean.cruz@shift.com.br>
2021-07-10 10:54:38 -0300

    capturing form information using ngSubmit

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,11 +1,16 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector:'app-nova-transferencia',
-    templateUrl:'./nova-transferencia.component.html',
-    styleUrls:['./nova-transferencia.component.scss']
-
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
 
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log(this.valor+' '+this.destino);
+  }
 }
