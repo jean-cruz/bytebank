f89a894c5403944e35dbaea43bec55237b3daca3
Jean Cruz <jean.cruz@shift.com.br>
2021-07-13 20:59:53 -0300

    created a service class for transfers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { TransferenciaService } from './services/transferencia.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,12 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'bytebank';
 
-  transferencias: any[] = [];
+  constructor(private service: TransferenciaService){}
 
   transferir($event){
-    const transferencia = {...$event, data: new Date()}
-    console.log(transferencia);
-
-    this.transferencias.push(transferencia);
+    this.service.adicionar($event);
   }
 }
